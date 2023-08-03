79ab67e024a6b078f41111f23b4095356f93a811
Satyum Singh Pathania <satyumspathania@gmail.com>
2023-08-03 06:46:31 +0000

    React Styling Practise

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import "./styles.css";
 
 export default function App() {
+  const date = new Date();
+  const currTime = date.getHours();
+
+  var greeting;
+
+  var customStyle = {
+    color: "yellow",
+  };
+
+  if (currTime > 0 && currTime <= 12) {
+    greeting = "Good Morning";
+    customStyle.color = "red";
+  }
+  if (currTime > 12 && currTime <= 18) {
+    greeting = "Good Afternoon";
+    customStyle.color = "green";
+  }
+  if (currTime > 18 && currTime <= 24) {
+    greeting = "Good Evening";
+    customStyle.color = "blue";
+  }
   return (
     <div className="App">
-      <h1>My favourite items</h1>
-      <ul>
-        <li>Food</li>
-        <li>Travelling</li>
-        <li>Gaming</li>
-      </ul>
+      <h1 style={customStyle}>{greeting}</h1>
     </div>
   );
 }
